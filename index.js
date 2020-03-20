5c293e9dfcab64f5944aa216c0b8bc18040994c1
AQadir64 <a.qadir67@outlook.com>
2020-03-20 20:21:46 +0500

    store dispatch and reducer

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function createStore() {
+function createStore(reducer) {
   //four parts of the store
   // 1) state of the store
   // 2) get the state
@@ -19,9 +19,17 @@ function createStore() {
     };
   };
 
+  const dispatch = action => {
+    state = reducer(state, action);
+    listeners.forEach(listen => {
+      listen();
+    });
+  };
+
   return {
     getState,
-    subscribe
+    subscribe,
+    dispatch
   };
 }
 
@@ -42,9 +50,17 @@ function createStore() {
 // The type property is set as action of the todo event
 // When an event takes place in a Redux application, we use a plain JavaScript object to keep track of what the specific event was.
 // This object is called an Action.
+// 2) the function that return the new state must be a pure function
+// the most important part of the pure function are that they are predictable
 
+function todos(state = [], action) {
+  if (action.type === "ADD_TODO") {
+    state.concat([action.todo]);
+  }
+  return state;
+}
 
-const store = createStore();
+const store = createStore(todos);
 
 store.subscribe(() => {
   console.log("The new state is :", store.getState);
